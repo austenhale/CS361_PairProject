de0046273600b250a8cfba84909e359c6fa03252
auste <auste@10.101.153.1>
2020-09-19 14:38:54 -0600

    Adding complement method

--- CS361/fa/dfa/DFA.java
+++ CS361/fa/dfa/DFA.java
@@ -9,10 +9,9 @@ import fa.State;
 
 public class DFA implements DFAInterface{
 	
-	private HashSet<DFAState> statesSet; //holds all the states of the DFA
-	
+	private HashSet<DFAState> statesSet = new HashSet<>(); //holds all the states of the DFA
 	private DFAState startState; //starting state
-	private HashSet<DFAState> finalStates; //holds all final states
+	private HashSet<DFAState> finalStates = new HashSet<>();; //holds all final states
 
 	@Override
 	public void addStartState(String name) {
@@ -71,8 +70,32 @@ public class DFA implements DFAInterface{
 
 	@Override
 	public DFA complement() {
-		// TODO Auto-generated method stub
-		return null;
+		DFA complement = new DFA();
+		HashSet<DFAState> statesSetComplement = new HashSet<>(); //holds all the states of the DFA
+		DFAState startStateComplement = startState; //starting state
+		HashSet<DFAState> finalStatesComplement = new HashSet<>(); //holds all final states
+		
+		//Building up lists
+		for (DFAState state : statesSet) { //go through every state in original
+			if (finalStates.contains(state)) { //if the original state had this state as a final state
+				statesSetComplement.add(state); //add it to just the states set
+			}else { //else if it wasn't in final states (i.e. wasn't a final state of the original)
+				finalStatesComplement.add(state); //add it to the complement's final states
+				statesSetComplement.add(state); //also add final state to overall state collection
+			}
+			
+		}
+		
+		//setting lists
+		for (DFAState state : finalStatesComplement) {
+			complement.addFinalState(state.getName());
+		}
+		complement.addStartState(startStateComplement.getName());
+		for (DFAState state : statesSetComplement) {
+			complement.addState(state.getName());
+		}
+		
+		return complement;
 	}
 
 	@Override
