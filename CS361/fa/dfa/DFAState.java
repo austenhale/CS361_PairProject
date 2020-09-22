a6cf3c7fa2d2cc910d3c507112f7bc5988586734
auste <auste@10.101.153.1>
2020-09-22 14:13:15 -0600

    jk moved alphabet to DFA

--- CS361/fa/dfa/DFAState.java
+++ CS361/fa/dfa/DFAState.java
@@ -10,22 +10,17 @@ public class DFAState extends State{
 	
 	private String name;
 	private HashMap<Character, State> transitionMap; //holds all transitions, unsure if correct paramters
-	private HashSet<Character> alphabet;
+	
 
 	public DFAState(String name) {
 		this.name = name;
-		alphabet = new HashSet<>();
 		transitionMap = new HashMap<Character, State>();
 	}
 
 	public void addTransition(char onSymb, State toState) {
 		transitionMap.put(onSymb, toState);
-		alphabet.add(onSymb);
 		
 	}
-	
-	public Set<Character> getAlphabet() {
-		return alphabet;
-	}
+
 
 }
