63f6ece1f647ab8b992331c867dbef34ebcf7fab
auste <auste@10.101.153.1>
2020-09-22 14:11:16 -0600

    Update to DFAState to include alaphabet

--- CS361/fa/dfa/DFAState.java
+++ CS361/fa/dfa/DFAState.java
@@ -1,6 +1,8 @@
 package fa.dfa;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import fa.State;
 
@@ -8,14 +10,22 @@ public class DFAState extends State{
 	
 	private String name;
 	private HashMap<Character, State> transitionMap; //holds all transitions, unsure if correct paramters
+	private HashSet<Character> alphabet;
 
 	public DFAState(String name) {
 		this.name = name;
+		alphabet = new HashSet<>();
+		transitionMap = new HashMap<Character, State>();
 	}
 
 	public void addTransition(char onSymb, State toState) {
 		transitionMap.put(onSymb, toState);
+		alphabet.add(onSymb);
 		
+	}
+	
+	public Set<Character> getAlphabet() {
+		return alphabet;
 	}
 
 }
