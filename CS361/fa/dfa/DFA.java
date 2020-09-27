226d51bbb97433ba410059459cd9f9fe2ee03af5
auste <auste@10.101.153.1>
2020-09-27 12:14:26 -0600

    Javadocs and readme

--- CS361/fa/dfa/DFA.java
+++ CS361/fa/dfa/DFA.java
@@ -93,19 +93,9 @@ public class DFA implements DFAInterface{
 	@Override
 	public DFA complement() {
 		DFA complement = new DFA();
-	
-		
-		
-		//Building up lists
 
-		
-		//add all the non final states from org as final states to complement
-			//add all those final states to the state list at same time
-		//add start state
-		//add the old final states to states list
-		
 		//setting lists
-		
+		//add all the non final states from org as final states to complement
 		for (DFAState state : statesSet) {
 			if (!finalStates.contains(state)) {
 				complement.addFinalState(state.getName());
@@ -114,31 +104,31 @@ public class DFA implements DFAInterface{
 			
 		}
 		
+		//add start state
 		complement.addStartState(startState.getName());
 		
+		//add the old final states to states list
 		for (DFAState state : finalStates) {
 				complement.addState(state.toString());
 			
 		}
 		
 		
-//		
-		for (DFAState state: statesSet) {
-			HashMap<Character, DFAState> complementMap = state.getMap();
-			Iterator mapIterator = complementMap.entrySet().iterator();
-			while (mapIterator.hasNext()) {
-				Map.Entry mapElement = (Map.Entry)mapIterator.next();
-				DFAState stateTwoName = (DFAState)mapElement.getValue();
-				String stateOne = state.getName();
-				Character chara = (Character)mapElement.getKey();
-				String stateTwo = get(stateTwoName, statesSet).getName();
-				complement.addTransition(stateOne, chara, stateTwo);
+		//Iterate through all the DFAStates, and add their transitions to the complement
+		for (DFAState state: statesSet) { //go through every DFAState
+			HashMap<Character, DFAState> complementMap = state.getMap(); //get that DFAState's transition map
+			Iterator mapIterator = complementMap.entrySet().iterator(); //create an iterator for the map
+			while (mapIterator.hasNext()) { //go through all transitions that were added to the DFAState, check if there is a next transition
+				Map.Entry mapElement = (Map.Entry)mapIterator.next(); //go to the next transition
+				DFAState stateTwoName = (DFAState)mapElement.getValue(); //get the value (value will be a DFAState)
+				String stateOne = state.getName(); //name received for use in addTransition method
+				Character chara = (Character)mapElement.getKey(); //get the character for transition
+				String stateTwo = get(stateTwoName, statesSet).getName(); //get the toState
+				complement.addTransition(stateOne, chara, stateTwo); //add the transition to the complement
 
-				
-				
 			}
 		}
-//			//get transition given state and character
+
 		
 		return complement;
 	}
@@ -156,24 +146,24 @@ public class DFA implements DFAInterface{
 		//check if current state is a final state
 			//if it is, return true, else false
 		DFAState currentState = null;
-		if (get(startState, statesSet) != null) {
-			currentState = get(startState, statesSet);
+		if (get(startState, statesSet) != null) { 
+			currentState = get(startState, statesSet); //update currentState to be startState
 		}else {
-			return false;
+			return false; //if startState isn't in stateSet, doesn't accept string
 		}
 		
 		State nextState;
 		for (int i = 0; i < s.length(); i++) {
-			nextState = getToState(currentState, s.charAt(i));
+			nextState = getToState(currentState, s.charAt(i)); //update nextState to be the transition of currentState given a character
 			if (nextState == null && s.charAt(i) != 'e') { //if invalid move and the character isn't an empty string
 				return false;
 			}
-			if (s.charAt(i) != 'e') {
+			if (s.charAt(i) != 'e') { //corner case for if user passes in only empty string
 				currentState = (DFAState) nextState;
 			}
 			
 		}
-		if (finalStates.contains(currentState)) {
+		if (finalStates.contains(currentState)) { //if they ended on a final state, return true
 			return true;
 		}
 		return false;
@@ -183,7 +173,15 @@ public class DFA implements DFAInterface{
 	public State getToState(DFAState from, char onSymb) {
 		return from.transition(onSymb);
 	}
-	
+	/**
+	 * Returns a String in the format with the following order:
+	 * Q = {Whole set of States}
+	 * Sigma = {alphabet}
+	 * Delta = {transitions}
+	 * q0 = {start state}
+	 * F = {final state}
+	 * return string with specified format
+	 */
 	public String toString() {
 		String formatedString = "";
 	
@@ -240,10 +238,10 @@ public class DFA implements DFAInterface{
 	}
 	
 	/**
-	 * This will get an element from the set
+	 * This will get an element from the set.
 	 * @param stateToGet
 	 * @param set
-	 * @return
+	 * @return a copy of the State from the set, null if state is not in set
 	 */
 	private DFAState get(DFAState stateToGet, Set<DFAState> set) {
 		for (DFAState state : set) {
--- CS361/fa/dfa/DFAState.java
+++ CS361/fa/dfa/DFAState.java
@@ -6,6 +6,12 @@ import java.util.Set;
 
 import fa.State;
 
+/**
+ * This class implements a DFAState, which holds each transition for a state, as well as each
+ * DFAState's name.
+ * @author austen hale, sammie fullmer
+ *
+ */
 public class DFAState extends State{
 	
 	private String name;
@@ -17,10 +23,20 @@ public class DFAState extends State{
 		transitionMap = new HashMap<Character, DFAState>();
 	}
 
+	/**
+	 * Adds a transition to the DFAState.
+	 * @param onSymb - character required to transition to toState
+	 * @param toState - state that will be transition to.
+	 */
 	public void addTransition(char onSymb, DFAState toState) {
 		transitionMap.put(onSymb, toState);
 		
 	}
+	/**
+	 * Returns the state that is transitioned to with the given character.
+	 * @param onSymb - character required to transition to state
+	 * @return - returns state that is reached by onSymb, null if no valid transition state
+	 */
 	public State transition(char onSymb) {
 		if (transitionMap.containsKey(onSymb)) {
 			return transitionMap.get(onSymb);
@@ -36,6 +52,10 @@ public class DFAState extends State{
 		return name;
 	}
 	
+	/**
+	 * Returns the map containing the transitions.
+	 * @return transitionMap
+	 */
 	public HashMap getMap() {
 		return transitionMap;
 	}
